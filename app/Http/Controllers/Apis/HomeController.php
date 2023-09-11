bcbab3ce95b099f1b013256c0abed0c50a3451ad
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-09-11 12:40:25 +0300

    api shape

--- app/Http/Controllers/Apis/HomeController.php
+++ app/Http/Controllers/Apis/HomeController.php
@@ -2,21 +2,26 @@
 
 namespace App\Http\Controllers\Apis;
 
+
 use App\Models\Brand;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Catigory;
 use App\Models\Subcatigory;
+use App\Traits\ApiResponses;
 
 class HomeController extends Controller
 {
+
+    use ApiResponses;
     public function index()
     {
         $brands = Brand::all();
         $newProduct = Product::select('image', 'en_name', 'en_details')->orderBy('created_at')->first();
         $categories = Catigory::select('en_name', 'id')->get();
         $subcategories = Subcatigory::select('en_name', 'id')->get();
-        return response()->json(compact('brands', 'newProduct', 'categories', 'subcategories'));
+        // return response()->json(compact('brands', 'newProduct', 'categories', 'subcategories'));
+        return $this->data(compact('brands', 'newProduct', 'categories', 'subcategories'), 'done');
     }
 }
--- app/Http/Controllers/Apis/ProductController.php
+++ app/Http/Controllers/Apis/ProductController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers\Apis;
 
+
 use App\Http\Controllers\Controller;
+
 use App\Models\Product;
+use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    use ApiResponses;
+
     public function index()
     {
         $products = Product::all();
-        return response()->json(compact('products'));
+
+        return $this->data(compact('products'), 'done');
     }
 }
