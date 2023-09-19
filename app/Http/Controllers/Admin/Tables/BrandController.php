f21eee3c2cb87bb5a3dca70e41ecb5adbe59f972
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-09-19 03:36:25 +0300

    edit and delete brands

--- app/Http/Controllers/Admin/Tables/BrandController.php
+++ app/Http/Controllers/Admin/Tables/BrandController.php
@@ -42,9 +42,51 @@ class BrandController extends Controller
         ]);
         return redirect(route('brands.index'))->with('success', 'the brand create successfully');
     }
-    // public function edit($id)
-    // {
-    //     $brands = DB::table('brands')->where('id', '=', $id)->first();
-    //     return view('CRUD.Brands.edit', compact('brands'));
-    // }
+    public function edit($id)
+    {
+        $brands = Brand::where('id', '=', $id)->first();
+        return view('admin.CRUD.Brand.edit', compact('brands'));
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'en_name' => 'required|max:32',
+            'ar_name' => 'required|max:32',
+            'status' => 'required|in:0,1',
+            'image' => 'image'
+        ]);
+        $dbimage = Brand::select('image')->where('id', '=', $id)->first();
+        $imageName = $dbimage->image;
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $extention = $image->getClientOriginalExtension();
+            $imageName = 'brands-' . uniqid() . '.' . $extention;
+            $path = public_path('images\brand-logo');
+            $dbimage = Brand::select('image')->where('id', '=', $id)->first();
+            $oldpath1 = $path . '\\' . $dbimage->image;
+            if (file_exists($oldpath1)) {
+                unlink($oldpath1);
+            }
+            $image->move($path, $imageName);
+        }
+        Brand::where('id', '=', $id)->update([
+            'en_name' => $request->en_name,
+            'ar_name' => $request->ar_name,
+            'status' => $request->status,
+            'image' => $imageName,
+        ]);
+        return  redirect(route('brands.index'))->with('success', 'the brand updated successfully');
+    }
+    public function delete($id)
+    {
+        $path = public_path('images\brand-logo');
+        $dbimage = Brand::select('image')->where('id', '=', $id)->first();
+        $oldpath1 = $path . '\\' . $dbimage->image;
+        if (file_exists($oldpath1)) {
+            unlink($oldpath1);
+        }
+
+        Brand::where('id', '=', $id)->delete();
+        return back()->with('success', 'the brand deleted successfully');
+    }
 }
