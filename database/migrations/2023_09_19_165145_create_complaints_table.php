8263c498a00525d0ec7a79425675f42e32a82185
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-09-19 19:54:31 +0300

    complaaints table

---
+++ database/migrations/2023_09_19_165145_create_complaints_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name', 32);
+            $table->string('user_email', 100);
+            $table->string('user_phone', 11);
+            $table->string('subject', 255);
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complaints');
+    }
+};
